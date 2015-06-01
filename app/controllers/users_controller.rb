28caed4aa45510e627019a9845a1bf6c193db617
mcooper <mikeyjc@gmail.com>
2015-06-01 19:00:06 +1000

    Added sign-up form/log-in capabilities

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,5 @@
 class UsersController < ApplicationController
-
+  
   before_action :check_if_admin, :only => [:index]
   def index
     @users = User.all
--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def intellinav
+    nav = ''
+    if @current_user.present? && @current_user.admin?
+      nav+= '<li>' + link_to('Show users', users_path) + '</li>'
+    end
+    if @current_user.present?
+      nav += '<li>' + link_to("Edit profile", edit_users_path)
+      nav += '<li>' + link_to("Log Out #{@current_user.name}", login_path, :method => :delete, :confirm => 'Are you sure?')  + '</li>'
+    else
+      nav +=  '<li>' + link_to('Sign Up', new_user_path) + '</li>'
+      nav +=  '<li>' + link_to('Login', login_path) + '</li>'
+    end
+    nav
+  end
 end
